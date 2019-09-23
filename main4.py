ae2a887adf887995921706f367f0b63b42d38d72
tjhosek <tjhosek@iu.edu>
2019-09-23 17:34:44 -0400

    Finished main4, imported new assets

--- main4.py
+++ main4.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import utils, os, random, time, open_color, arcade
+import utils, os, random, time, open_color, arcade, math
 
 utils.check_version((3,7))
 
@@ -8,6 +8,15 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Sprites Example"
 
+# This code was addapted from kyong.com/python/python-how-to-list-all-files-in-a-directory/
+# It puts every file in a directory in a list
+png_list = []
+
+for r, d, f in os.walk("assets/kenney_tankspack/PNG/Default_size"):
+    for file in f:
+        if '.png' in file:
+            png_list.append(os.path.join(r, file))
+# This is the end of the adapted
 
 class MyGame(arcade.Window):
 
@@ -17,21 +26,34 @@ class MyGame(arcade.Window):
         os.chdir(file_path)
         arcade.set_background_color(open_color.white)
 
+        self.sprite_list = arcade.SpriteList()
+
 
     def setup(self):
-        pass        
+        for i in range(20):
+            self.sprite = arcade.Sprite(random.choice(png_list))
+            self.sprite.center_x = random.randint(0,SCREEN_WIDTH)
+            self.sprite.center_y = random.randint(0,SCREEN_HEIGHT)
+            self.sprite_list.append(self.sprite)
 
     def on_draw(self):
         arcade.start_render()
-        pass
-
+        self.sprite_list.draw()
 
     def update(self, delta_time):
         pass
 
-
     def on_mouse_motion(self, x, y, dx, dy):
-        pass
+        for i in self.sprite_list:
+            if math.sqrt((x-self.sprite.center_x)**2 + (y-self.sprite.center_y)**2) < 10:
+                if self.sprite.center_x > x:
+                    self.sprite.center_x+=1
+                else:
+                    self.sprite.center_x-=1
+                if self.sprite.center_y > y:
+                    self.sprite.center_y+=1
+                else:
+                    self.sprite.center_y-=1
 
 def main():
     """ Main method """
